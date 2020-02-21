d1784f48deff8d310ec46f473ee97aec9c8d75ef
Tamtnguye <apple@apples-mbp.wifi.bcit.ca>
2020-02-21 08:46:00 -0800

    added sound and overlap

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
+    width: 1000,
     height: 600,
     backgroundColor: "#5f2a55",
     scene: {
@@ -28,19 +28,37 @@ function preload() {
     this.load.spritesheet('characters', '/images/characters.png', { frameWidth: 32, frameHeight: 32 });
 
     this.load.spritesheet('pokemon', '/images/images.png', { frameWidth: 67, frameHeight: 67 });
+
+    this.load.audio('goldSound', ['/audio/Pickup.wav']);
 };
 
 function create() {
+
+    const goldPickAudio = this.sound.add('goldSound', {loop: false, volume: 0.2});
+    
     var button = this.add.image(200, 200, 'button1');
     button.setOrigin(0.5, 0.5);
     this.add.sprite(300, 100, 'button1');
-    this.add.image(200, 250, 'items', 0);
-    this.physics.add.image(500, 100, 'button1');
-    this.add.sprite(400, 300, 'pokemon', 0);
+    
+    this.chest = this.physics.add.image(200, 250, 'items', 0);
+    
+    this.wall = this.physics.add.image(500, 100, 'button1');
+    this.wall.setImmovable();
+    
+    this.pokemon = this.physics.add.sprite(400, 300, 'pokemon', 0);
 
 
     this.player = this.physics.add.image(32, 32, 'characters', 2);
     this.player.setScale(2);
+    this.player.body.setCollideWorldBounds(true);
+
+    // physics
+    //this.physics.add.collider(this.player, this.wall); this will make the object move and run away from the point of contact. and disappear. 
+    //  this.wall.setImmovable(); this will prevent the object from moving at all.
+    this.physics.add.collider(this.player, this.wall);
+
+    this.physics.add.overlap(this.player, this.chest, function(player, chest) {goldPickAudio.play(); chest.destroy();});
+    this.physics.add.overlap(this.player, this.pokemon, function() {console.log('overlap'); });
 
     this.cursors = this.input.keyboard.createCursorKeys();
 
