32ba7ba5ac52724d9d5df36cb2a76f0ced1f5171
Tamtnguye <apple@apples-mbp.wifi.bcit.ca>
2020-02-26 07:37:13 -0800

    notes

--- public/js/classes/UiButton.js
+++ public/js/classes/UiButton.js
@@ -1,5 +1,37 @@
 class UiButton extends Phaser.GameObjects.Container {
     constructor(scene, x, y, key, hoverKey, text, targetCallback) {
         super(scene, x, y);
+
+        this.scene = scene;// the scene this container will be added to 
+        this.x = x; // the x position of our container
+        this.y = y; // the y position
+        this.key = key; // the background image of our button
+        this.hoverKey = hoverKey; // the image that will be displayed when the player hover over the button
+        this.text = text; // the text that will be displayed on the button
+        this.targetCallback = targetCallback ; // the callback function that will be called when the player clicks button
     }
 }
+
+//create play game button
+this.button = this.add.image(this.scale.width / 2, this.scale.height * 0.65, 'button1');
+
+this.button.setInteractive();
+
+this.buttonText = this.add.text(0, 0, 'Start', { fontSize: '26px', fill: "#fff" });
+
+//to align text into objects
+Phaser.Display.Align.In.Center(this.buttonText, this.button);
+this.button.on('pointerdown', () => {
+    this.scene.start('Game')
+});
+
+this.button.on('pointerover', () => {
+    
+    this.button.setTexture('button2');
+});
+
+this.button.on('pointerout', () => {
+    
+    this.button.setTexture('button1');
+})
+
--- public/js/scenes/TitleScene.js
+++ public/js/scenes/TitleScene.js
@@ -10,28 +10,5 @@ class TitleScene extends Phaser.Scene {
         this.titleText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'Thirteen mmo', { fontSize: '64px', fill: '#fff' });
         this.titleText.setOrigin(0.5);
 
-        //create play game button
-        this.button = this.add.image(this.scale.width / 2, this.scale.height * 0.65, 'button1');
-
-        this.button.setInteractive();
-
-        this.buttonText = this.add.text(0, 0, 'Start', { fontSize: '26px', fill: "#fff" });
-
-        //to align text into objects
-        Phaser.Display.Align.In.Center(this.buttonText, this.button);
-        this.button.on('pointerdown', () => {
-            this.scene.start('Game')
-        });
-
-        this.button.on('pointerover', () => {
-            
-            this.button.setTexture('button2');
-        });
-
-        this.button.on('pointerout', () => {
-            
-            this.button.setTexture('button1');
-        })
-        
     }
 }
