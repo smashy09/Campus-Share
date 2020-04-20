fbdcb98dd6ad8aac9e1af38f5fd792e38231d47b
Tamtnguye <apple@Apples-MBP.hitronhub.home>
2020-04-20 11:18:29 -0700

    deleted ejs stuff

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 const express = require("express");
 const PORT = process.env.port || 5000;
 
+const mongoose = require("mongoose")
 const app = express();
 
-const server = require('http').Server(app);
 
-// app.get('/game.html', function (req, res) {
-//   res.sendFile(__dirname + '/public/game.html');
-// });
+
+
+//DB config
+const db = require("./config/keys").MongoURI;
+//connect to mongo
+
+// mongoose.connect(db, { useNewUrlParser: true})
+// .then(() => console.log('MongoDb Connected...'))
+// .catch(err => console.log(err));
+
+
+
+//bodyparse
+app.use(express.urlencoded({ extended: false}));
+
+
+// game
+app.get('/game.html', function (req, res) {
+  res.sendFile(__dirname + '/public/game.html');
+});
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.use('/', require('./public/'));
+
+
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 app.use((req, res, next) => {
   res.status(404).json({ message: '404 - Not Found' });
@@ -22,6 +42,6 @@ app.use((req, res, next) => {
 //     console.log("server has started!");
 // });
 
-server.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server started on port ${process.env.PORT || 5000}`);
 });
