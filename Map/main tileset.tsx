f4d42c95aac45736d1f398117589be09ae2aef0e
Justin Namoro <justinnamoro@JustinsMac.local>
2020-04-29 12:40:21 -0700

    map v2

---
+++ Map/main tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="main tileset" tilewidth="32" tileheight="32" tilecount="1096" columns="8">
+ <image source="tileset.png" width="265" height="4400"/>
+</tileset>
